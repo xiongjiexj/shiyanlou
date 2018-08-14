d7156cc95de466ec9bfe47491db803393f8dcc90
xiongjiexj <1193304428@qq.com>
2018-08-14 23:17:49 +0800

    solarSystem

---
+++ glut/soloarSystem.hpp
@@ -0,0 +1,17 @@
+#define STAR_NUM 10
+#define TIMEPAST 1
+#define SELFROTATE 3
+enum STARS{
+	Sun,
+	Mercury,
+	Venus,
+	Earth,
+	Moon,
+	Mars,
+	Jupiter,
+	Saturn,
+	Uranus,
+	Neptune
+}
+
+Star *stars[START_NUM];
